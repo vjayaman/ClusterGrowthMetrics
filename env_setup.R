e13387f82c76ff806c62fdf03702d6538d5737b4
vjayaman <vjayaman314@gmail.com>
2020-10-21 13:26:46 -0500

    Initialization prep

---
+++ env_setup.R
@@ -0,0 +1,11 @@
+
+
+install.packages("tibble")
+install.packages("magrittr")
+install.packages("dplyr")
+install.packages("reshape2")
+install.packages("scales")
+
+dir.create("data", showWarnings = FALSE)
+
+# Now move the data you want to use to the newly created "data" directory
