7d47a60136d8ac2d6fc7d695879b1c591e931c02
vjayaman <vjayaman314@gmail.com>
2020-12-03 22:15:35 -0600

    After datacollection, rewrote the preparingmetrics section, dealt with the merging issue - still would like to test more extensively, currently 95% confident in the results

--- preparingmetrics.R
+++ preparingmetrics.R
@@ -2,6 +2,7 @@
 source("flagging_functions.R")
 pb <- progress_bar$new(total = 22)
 pb$tick()
+
 # the cluster assignments, in form: || isolates | height 0 | height 1 | ... ||
 # the raw datasets, no filtering or other changes made
 time1_raw <- "data/timepoint1_data.csv" %>% readData(., X)
@@ -10,49 +11,117 @@ time2_raw <- "data/timepoint2_data.csv" %>% readData(., Y)
 # USER: make sure the first column is the isolate labeling
 # then replace with "isolate" for easier manipulation later on
 colnames(time1_raw)[1] <- colnames(time2_raw)[1] <- "isolate"
-pb$tick()
+
 # isolates found at TP2 (both original and novel isolates) - found at TP1 (the original isolates):
 # isolates introduced at TP2 (novel isolates):
 novels <- setdiff(time2_raw$isolate, time1_raw$isolate)
 pb$tick()
+
 # tracked_clusters is the list of original genomes and their cluster assignments at all heights as well 
 # as their originating clusters, with clusters flagged if their originating cluster has been found before
 # the clusters that actually changed from TP1 to TP2 are flagged with an NA
 
-original_tracking <- readRDS("outputs/results.Rds") %>% 
+# checking the clusters of the results from datacollection.R
+original_tracking <- readRDS("outputs/results.Rds") %>% createID(., "tp2_h", "tp2_cl")
+pb$tick()
+
+every_case <- time2_raw %>% 
+  melt(., id = "isolate") %>% as_tibble() %>% 
+  set_colnames(c("isolate","tp2_h", "tp2_cl")) %>% 
+  arrange(isolate, tp2_h, tp2_cl) %>% 
+  mutate(across(tp2_h, as.character)) %>% 
+  mutate(across(tp2_h, as.integer)) %>% 
   createID(., "tp2_h", "tp2_cl")
-pb$tick()
-# in this dataset we have, for all ORIGINAL isolates, the cluster assignments at TP1 and TP2, 
-# the change in cluster size, and whether or not the originating cluster was found before
 
-# the next step:
-#   - for each novel isolate
-#     - get the cluster assignments at all heights (for TP2)
-#       - then match with the originating cluster at TP1 (if one existed)
-#       - use the originating cluster set (so the flags are included)
-#     - get the cluster sizes (TP1 and TP2)
-# then:
-#   - proportional increase, adaptive threshold, fold change
+# check1a <- every_case %>% 
+#   filter(!(isolate %in% novels))
+# 
+# check1b <- original_tracking %>% 
+#   select(isolate, tp2_h, tp2_cl) %>% 
+#   arrange(isolate, tp2_h, tp2_cl) %>% 
+#   createID(., "tp2_h", "tp2_cl")
+# 
+# identical(check1a, check1b)
 
-# input: time2_raw, novels, result data (original_tracking)
-# This part is just to check that all the TP2 clusters are represented
-checkForMissing(time2_raw, original_tracking, chktype = 1, novels)
-pb$tick(len = 3)
+# cl_w_orig <- check1a$id %>% unique()
+# TRUE --> in the results file, we have all the TP2 cluster assignments for the original isolates as
+# well as the metrics of their originating clusters and flags for when they first emerged from a
+# TP1 cluster
 
-tracked <- time2_raw %>% addNovelsToResults(., novels) %>% add_column(prop_inc = NA)
-# CHECKING: the following should be true, to show that we didn't lose any clusters on the way so far
-# identical(sort(unique(all_sizes$id)), sort(unique(tracked$id)))
+# WHAT WE NEED TO ADD:
+# information regarding novels
+#   a) clusters that contain both originals and novels (those that absorbed novels at TP2)
+#     - we should have the origin information for these from the results file, we just need
+#     to merge this data with that of the novels absorbed into their respective clusters
+#   b) clusters that are entirely composed of novels at TP2 (i.e. did not exist at TP1)
 
-# HANDLE THESE CASES SEPARATELY: 
-zero_denoms <- tracked %>% filter(tp1_cl_size == 0)
-pb$tick()
-# we now are only going to look at clusters that existed at TP1 first
-# we'll add the zero_denominator cases back in later
-tracked <- tracked %>% filter(tp1_cl_size > 0)
-pb$tick()
-tracked$prop_inc <- (tracked$tp2_cl_size - tracked$tp1_cl_size) / tracked$tp1_cl_size
-pb$tick()
-## Adaptive threshold development
+# PART 1: novel only TP2 clusters
+nov_cl <- every_case %>% 
+  filter(isolate %in% novels)
+
+orig_cl <- every_case %>% 
+  filter(!(isolate %in% novels))
+
+# ----------------------------------------------------------------------------------------------------
+both <- every_case %>% 
+  filter(id %in% intersect(nov_cl$id, orig_cl$id))
+
+# check that all of these clusters have been properly tracked: 
+all(both$id %in% original_tracking$id)       # should be TRUE
+
+# removing the originals, since we already have that information in original_tracking
+both_n <- both %>% filter(isolate %in% novels)
+oan <- original_tracking %>% select(-isolate) %>% 
+  filter(id %in% both$id) %>% unique() %>% 
+  left_join(both_n, ., by = c("tp2_h", "tp2_cl", "id"))
+
+
+metrics <- original_tracking %>% bind_rows(., oan)
+
+# ----------------------------------------------------------------------------------------------------
+nov_only <- every_case %>% 
+  filter(id %in% nov_cl$id) %>% 
+  filter(!(id %in% orig_cl$id))
+
+# checking that these are clusters we haven't dealt with yet
+any(nov_only$id %in% metrics$id)
+# checking that all the isolates are not present in TP1
+any(nov_only$isolate %in% time1_raw$isolate)
+
+nov_only <- nov_only %>% group_by(tp2_h, tp2_cl) %>% 
+  summarise(tp2_cl_size = n(), .groups = "drop") %>% 
+  left_join(nov_only, ., by = c("tp2_h", "tp2_cl")) %>% 
+  arrange(tp2_h, tp2_cl)
+
+nov_only <- nov_only %>% 
+  add_column(tp1_h = NA, tp1_cl = NA, tp1_cl_size = 0, flagged = NA) %>% 
+  select(colnames(metrics))
+
+# checking that these clusters have not been accounted for
+any(nov_only$id %in% metrics$id)
+
+metrics <- metrics %>% bind_rows(., nov_only)
+
+# checking that all TP2 clusters have been now accounted before, and that there 
+# are no repeats or other anomalies
+all(every_case$id %in% metrics$id)
+a <- metrics$id %>% sort()
+b <- every_case$id %>% sort()
+identical(a, b)
+
+# TWO CASES FOR PROPORTIONAL INCREASE HANDLING
+#   a) clusters did not exist at TP1 (denominator of zero)
+#     --> prop_inc = NA
+#   b) clusters did exist at TP1
+#     --> prop_inc of (TP2 size - TP1 size) / (TP1 size)
+
+metrics <- metrics %>% add_column(prop_inc = NA)
+
+case2 <- which(metrics$tp1_cl_size != 0)
+metrics$prop_inc[case2] <- metrics$tp2_cl_size[case2] %>% 
+  '-'(metrics$tp1_cl_size[case2]) %>% '/'(metrics$tp1_cl_size[case2])
+
+# ADAPTIVE THRESHOLD DEVELOPMENT
 
 # We can plot what a couple of different threshold models can look like.
 # This part will be heavily altered depending on what the actual data looks like.
@@ -62,44 +131,65 @@ pb$tick()
 # message("Running local regression steps for adaptive threshold development")
 x <- c(1, 25, 50, 100, 150)
 y <- c(300, 150, 75, 30, 15)
-final_df <- addPredictedToResults(x, y, tracked)
-pb$tick()
+
+# http://r-statistics.co/Loess-Regression-With-R.html
+loessMod1 <- loess(y ~ x, span = 1)
+smoothed1 <- predict(loessMod1)
+
+loessMod5 <- loess(y ~ x, span = 5)
+smoothed5 <- predict(loessMod5)
+
+lm_df <- tibble(x, y, smoothed1, smoothed5) %>%
+  set_colnames(c("x", "Preset values", "Local regression (span 1)", "Local regression (span 5)")) %>%
+  meltData(., "x") %>% set_colnames(c("Cluster size", "Function", "Growth"))
+
+# The model selection step, and using it to determine what the adaptive threshold
+# function values would be for each input of initial cluster size. Anything that
+# needs to be extrapolated is set to a pre-determined plateau value of percent increase.
+
+model_used <- loessMod1
+# predict.lm(model_used, data.frame(x = 30)) # note there are different types of prediction methods
+predicted_y <- predict(model_used, newdata = metrics$tp1_cl_size)
+na_predicted <- metrics$tp1_cl_size[which(is.na(predicted_y))]
+
+if (all(na_predicted > max(lm_df$`Cluster size`))) {
+  predicted_y[is.na(predicted_y)] <- 15
+}
+
+# FOLD CHANGE
+# # Ranking the data by the actual proportional change over the adaptive threshold requirement,
+# # to see by how much each cluster exceeds the growth prediction. The data is then saved to
+# # the current working directory.
+
+final_df <- predicted_y %>% round() %>% bind_cols(metrics, predicted = .)
+final_df$predicted <- final_df$predicted*0.01
+
 ## Fold change columns, convert decimals to percent
-final_df$fold_change <- (final_df$prop_inc / final_df$predicted) %>% round(., digits = 3)
-pb$tick()
-final_df <- final_df %>% 
-  arrange(., -fold_change) %>% 
-  mutate(across(c(predicted, prop_inc), scales::percent))
-pb$tick()
-# at this point, the columns we have data for are:
-#   - isolate
-#   - tp1_h, tp1_cl, tp1_cl_size, 
-#   - tp2_h, tp2_cl, tp2_cl_size, 
-#   - flagged, id, prop_inc, predicted, fold_change
+final_df$fold_change <- final_df$prop_inc %>% 
+  '/'(final_df$predicted) %>% 
+  round(., digits = 3)
+
+final_df <- final_df %>% arrange(., -fold_change)
+
+not_na <- which(!is.na(final_df$prop_inc))
+final_df$prop_inc[not_na] <- final_df$prop_inc[not_na] %>% scales::percent() 
+final_df$predicted[not_na] <- final_df$predicted[not_na] %>% scales::percent()
+
 # We still need a column with the number of novels in each TP2 cluster (note that this may not correspond 
 # exactly with the change in a cluster's size from TP1 to TP2)
-pb$tick()
-nov_sizes <- time2_raw %>% 
+nn <- every_case %>% 
   filter(isolate %in% novels) %>% 
-  clusterIDS() %>% 
-  select(-isolate, -id) %>%
+  select(tp2_h, tp2_cl) %>% 
   group_by_all() %>% 
-  summarise(num_novels = n(), .groups = "drop")
-pb$tick(len = 2)
-zero_denoms$fold_change <- zero_denoms$predicted <- NA
+  summarise(number_novels = n(), .groups = "drop") %>% 
+  createID(., "tp2_h", "tp2_cl")
 
-final_df <- final_df %>% 
-  left_join(., nov_sizes, by = c("tp2_h", "tp2_cl")) %>% 
-  bind_rows(., zero_denoms)
-pb$tick()
-checkForMissing(time2_raw, final_df, chktype = 2)
-pb$tick(len = 3)
-metrics <- final_df %>% select(isolate, tp1_h, tp1_cl, tp2_h, tp2_cl, tp1_cl_size, tp2_cl_size, 
-                               flagged, num_novels, prop_inc, predicted, fold_change) %>% 
+results <- final_df %>% left_join(nn, by = c("tp2_h", "tp2_cl", "id")) %>% 
+  select(isolate, tp1_h, tp1_cl, tp2_h, tp2_cl, tp1_cl_size, tp2_cl_size, 
+         flagged, number_novels, prop_inc, predicted, fold_change) %>% 
   set_colnames(c("Isolate", "Height (TP1)", "Cluster (TP1)", "Height (TP2)", "Cluster (TP2)", 
                  "TP1 cluster size", "TP2 cluster size", "Flag (originator seen before)", 
                  "Number of novels", "Proportional growth", "Adaptive threshold", 
                  "Fold change (Growth / Threshold)"))
-pb$tick()
-saveData(dtype = 4, m = metrics)
-pb$tick()
+
+saveData(dtype = 4, m = results)
