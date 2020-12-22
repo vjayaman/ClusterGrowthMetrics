2020397e27f32590bf5650f4a4e1326d4db68941
vjayaman <vjayaman314@gmail.com>
2020-12-22 09:51:11 -0600

    Reorganizing script to be more functional

--- dc2.R
+++ dc2.R
@@ -1,197 +1,17 @@
 x <- c("tibble", "magrittr", "dplyr", "reshape2", "scales", "progress", "stringr")
 lapply(x, require, character.only = TRUE)
 
-# given the defining filename, read in the data (need the full path from your working directory)
-readData <- function(filename, file_number) {
-  if (is.na(filename)) {
-    stop(paste0("Time point ", file_number, " dataset not found."))
-  }else {
-    read.csv(file = filename, stringsAsFactors = FALSE, numerals = "no.loss", 
-             check.names = FALSE) %>% as_tibble() %>% return()
-  }
-}
-
-factorToInt <- function(dataset, cname) {
-  dataset %>% 
-    mutate(across(all_of(cname), as.character)) %>% 
-    mutate(across(all_of(cname), as.integer)) %>% return()
-}
-
-countRows <- function(df, newname) {
-  df2 <- df %>% 
-    group_by(tp2_h, tp2_cl) %>% 
-    summarise(n = n(), .groups = "drop")
-  colnames(df2)[which(colnames(df2) == "n")] <- newname
-  df2 %>% return()
-}
-
-createID <- function(df, c1, c2) {
-  df %>% add_column(id = paste0(pull(df, c1), "-", pull(df, c2))) %>% return()
-}
-
-compsSet <- function(tp_coded, tp) {
-  cb <- tp_coded %>% set_colnames(c("isolate", "tp_h", "tp_cl", "id"))
-  a1 <- cb$tp_h %>% unique()
-  
-  pb <- txtProgressBar(min = 0, max = length(a1), initial = 0, style = 3)
-  tmp <- lapply(1:length(a1), function(i) {
-    setTxtProgressBar(pb, i)
-    x <- cb %>% filter(tp_h == a1[i]) %>% arrange(isolate)
-    if (length(unique(x$id)) > 1) {
-      a2 <- aggregate(isolate ~ id, data = x, FUN = toString) %>% as_tibble() %>% 
-        set_colnames(c("id", "composition"))
-      a3 <- aggregate(isolate ~ id, data = x, FUN = length) %>% as_tibble() %>% 
-        set_colnames(c("id", "size"))
-      left_join(a2, a3, by = "id") %>% return()
-    }else {
-      x$isolate %>% paste0(., collapse=",") %>% 
-        tibble(id = unique(x$id), composition = ., 
-               size = length(x$isolate)) %>% return()
-    }
-  }) %>% bind_rows()
-  close(pb)
-  
-  tpcomps <- str_split_fixed(tmp$id, "-", 2) %>% 
-    set_colnames(c("tp_h", "tp_cl")) %>% 
-    as_tibble() %>% 
-    bind_cols(., tmp) %>% 
-    mutate(across(c(tp_h, tp_cl), as.integer))
-  
-  if (tp == 1) {
-    tpcomps %>% rename(tp1_h = tp_h, tp1_cl = tp_cl, tp1_cl_size = size) %>% return()
-  }else {
-    tpcomps %>% rename(tp2_h = tp_h, tp2_cl = tp_cl, tp2_cl_size = size) %>% return()
-  }
-}
-
-noChange <- function(ca, cb, hb) {
-  in_both_heights <- ca %>% 
-    inner_join(., cb, by = "comp") %>% 
-    select(h_aft, cl_aft, size_aft, id_bef, id_aft)
-  
-  stayed_the_same <- left_join(in_both_heights, hb, by = c("id_bef" = "id")) %>% 
-    select(-tp1_h, -tp1_cl, -tp1_cl_size, -id_bef) %>% 
-    rename(tp1_h=h_aft, tp1_cl=cl_aft, tp1_cl_size=size_aft, id=id_aft) %>% 
-    select(colnames(hb))
-  stayed_the_same$flagged_heights <- stayed_the_same$flagged_heights + 1  
-  
-  # not found in TP2 in some capacity
-  inds <- which(is.na(stayed_the_same$tp2_h))
-  stayed_the_same$tp2_cl_size[inds] <- stayed_the_same$num_novs[inds] <- 0
-  return(stayed_the_same)
-}
-
-checkEachIsolate <- function(cluster_i, t2_coded, t2_comps) {
-  # no matching TP2 cluster found - the order of the cluster composition is muddling this up
-  if (length(grep(" ", cluster_i$composition)) == 0) {
-    isolates <- strsplit(cluster_i$composition, split = ",") %>% unlist()
-  }else {
-    isolates <- strsplit(cluster_i$composition, split = ", ") %>% unlist()  
-  }
-  ids <- t2_coded %>% filter(isolate %in% isolates) %>%
-    pull(id) %>% table() %>% 
-    as.data.frame() %>% as_tibble() %>% set_colnames(c("id", "size")) %>% 
-    filter(size == length(isolates)) %>% pull(id)
-  df2 <- t2_comps %>% filter(id %in% ids)
-  
-  cluster_i %>% rmIDComp() %>% bind_cols(., df2) %>% rmIDComp() %>% return()
-}
-
-
-rmIDComp <- function(df) {
-  df %>% select(-id, -composition) %>% return()
-}
-
-trackClusters <- function(hdata, t2_comps) {
-  # FOUND A PROBLEM WITH USING GREP!
-  #   - if you're looking for composition 1, it considers 10, 107, 1247, ... as hits
-  #   - need to include the commas in our actual checks
-  
-  singletons <- hdata %>% filter(tp1_cl_size == 1)
-  t1set <- t2set <- tibble()
-  if (nrow(singletons) > 0) { # at least one singleton cluster
-    # TP1 clusters that contain these TP1 singletons for this height
-    a1 <- t1_coded %>% filter(id %in% singletons$id) %>% rename(tp1_id = id)
-    # the TP2 clusters that contain at least the TP1 isolates from the above TP1 clusters
-    a2 <- t2_coded %>% filter(isolate %in% a1$isolate) %>% rename(tp2_id = id)
-    # the TP1 singletons matched to TP2 clusters that contain at least those genomes
-    a3 <- left_join(a1, a2, by = "isolate") %>% select(tp1_id, tp2_id)
-      
-    a4 <- t2_comps %>% filter(id %in% a3$tp2_id) %>% 
-      left_join(., a3, by = c("id" = "tp2_id")) %>% 
-      select(-composition, -id)
-    t1set <- singletons %>% rename(tp1_id = id) %>% 
-      select(-composition) %>% 
-      right_join(., a4, by = "tp1_id") %>% select(-tp1_id)
-  }
-  
-  hx <- hdata %>% filter(tp1_cl_size > 1)
-  if (nrow(hx) > 0) { # at least one cluster with size larger than 1
-    pb <- txtProgressBar(min = 0, max = nrow(hx) + 1, initial = 0, style = 3)
-    k <- 0
-    t2set <- lapply(1:nrow(hx), function(i) {
-      k <- k + i
-      setTxtProgressBar(pb, k)
-      cluster_i <- hx[i,]
-      
-      if (nchar(cluster_i$composition) > 2000) {
-        results_i <- checkEachIsolate(cluster_i, t2_coded, t2_comps)
-      }else {
-        results_i <- t2_comps[grep(cluster_i$composition, t2_comps$composition),] %>% 
-          rmIDComp() %>% bind_cols(cluster_i, .) %>% rmIDComp()
-        if (nrow(results_i) == 0) {
-          results_i <- checkEachIsolate(cluster_i, t2_coded, t2_comps)
-        }
-      }
-      return(results_i)
-    }) %>% bind_rows()
-    setTxtProgressBar(pb, k + 1)
-    close(pb)
-    if (any(is.na(t2set))) {message("\nNA values!\n")}
-  }
-  
-  if (nrow(t1set) > 0 & nrow(t2set) > 0) {
-    bind_rows(t1set, t2set) %>% return()
-  }else if (nrow(t1set) == 0 & nrow(t2set) > 0) {
-    t2set %>% return()
-  }else if (nrow(t1set) > 0 & nrow(t2set) == 0) {
-    t1set %>% return()
-  }else { # nrow(t1set) == 0 & nrow(t2set) == 0
-    message("No results!")
-  }
-}
-
-# --------------------------------------------------------------------------------------------------------
 # --------------------------------------------------------------------------------------------------------
 ## FOR USER: replace the filename variables with quoted file paths if you don't want to input them each time
 # # the cluster assignments, in form: || isolates | height 0 | height 1 | ... ||33333
 # # the raw datasets, no filtering or other changes made
 
-t1_coded <- "data/timepoint1_data.csv" %>% readData(., 1) %>% 
-  mutate(isolate = 1:nrow(.)) %>% 
-  melt(id = "isolate") %>% as_tibble() %>% 
-  set_colnames(c("isolate", "tp1_h", "tp1_cl")) %>% 
-  factorToInt("tp1_h") %>% 
-  createID(., "tp1_h", "tp1_cl")
-t1_coded$isolate <- paste0("-", t1_coded$isolate, "-")
+message("Reading timepoint 1 cluster assignment, processing...\n")
+t1_coded <- "data/timepoint1_data.csv" %>% readData(., 1) %>% codeIsolates(., "tp1")
+t1_comps <- t1_coded %>% rename(tp_h = tp1_h, tp_cl = tp1_cl) %>% compsSet(., 1)
 
-t2_coded <- "data/timepoint2_data.csv" %>% readData(., 2) %>% 
-  mutate(isolate = 1:nrow(.)) %>% 
-  melt(id = "isolate") %>% as_tibble() %>% 
-  set_colnames(c("isolate", "tp2_h", "tp2_cl")) %>% 
-  factorToInt("tp2_h") %>% 
-  createID(., "tp2_h", "tp2_cl")
-t2_coded$isolate <- paste0("-", t2_coded$isolate, "-")
-
-t1_comps <- t1_coded %>% rename(tp_h = tp1_h, tp_cl = tp1_cl) %>%
-  arrange(tp_h, tp_cl, isolate) %>%
-  compsSet(., 1)
-# # saveRDS(t1_comps, "t1_comps.Rds")
-# t1_comps <- readRDS("t1_comps.Rds")
-
-t2_comps <- t2_coded %>% rename(tp_h = tp2_h, tp_cl = tp2_cl) %>%
-  arrange(tp_h, tp_cl, isolate) %>%
-  compsSet(., 2)
+message("Reading timepoint 2 cluster assignment, processing...\n")
+t2_coded <- "data/timepoint2_data.csv" %>% readData(., 2) %>% codeIsolates(., "tp2")
 
 novels <- setdiff(t2_coded$isolate, t1_coded$isolate)
 counting_novels <- t2_coded %>%
@@ -199,9 +19,17 @@ counting_novels <- t2_coded %>%
   group_by(id) %>%
   summarise(num_novs = n(), .groups = "drop")
 
-t2_comps <- t2_comps %>% left_join(., counting_novels, by = "id")
+t2_comps <- t2_coded %>% 
+  rename(tp_h = tp2_h, tp_cl = tp2_cl) %>% 
+  compsSet(., 2) %>% 
+  left_join(., counting_novels, by = "id")
 t2_comps$num_novs[is.na(t2_comps$num_novs)] <- 0
-# # saveRDS(t2_comps, "t2_comps.Rds")
+
+# --------------------------------------------------------------------------------------------------------
+# saveData(dtype=2, tp=1, tpdata=t1_comps)
+# t1_comps <- readRDS("t1_comps.Rds")
+
+# saveData(dtype=2, tp=2, tpdata=t2_comps)
 # t2_comps <- readRDS("t2_comps.Rds")
 # --------------------------------------------------------------------------------------------------------
 # --------------------------------------------------------------------------------------------------------
@@ -227,7 +55,7 @@ cc$flag <- NA
 clx <- unique(cc$tp1_cl)
 k <- 0
 pb <- txtProgressBar(min = 0, max = length(clx), initial = 0, style = 3)
-hb <- lapply(1:length(clx), function(i) {
+hbdata <- lapply(1:length(clx), function(i) {
   k <- k + i
   setTxtProgressBar(pb, k)
   df <- cc %>% filter(tp1_cl == clx[i]) %>% arrange(tp2_h, tp2_cl)
@@ -242,12 +70,9 @@ close(pb)
 bef_comps <- t1_comps %>% filter(tp1_h == h_before) %>%
   set_colnames(c("h_bef", "cl_bef", "id_bef", "comp", "size_bef"))
 
-alldata <- hb
+alldata <- hbdata
 
-# saveRDS(alldata, "alldata.Rds")
-# saveRDS(hb, "hb.Rds")
-# saveRDS(h_before, "h_before.Rds")
-# saveRDS(bef_comps, "bef_comps.Rds")
+saveData(dtype=1, a=alldata, hbdata=hbdata, hb=h_before, cb=bef_comps)
 # --------------------------------------------------------------------------------------------------------
 # h4 is too slow - too many clusters? clusters too small? why is is so much slower than for h2 or h3?
 # --------------------------------------------------------------------------------------------------------
@@ -260,7 +85,7 @@ for (h_after in heights[-1]) {
     set_colnames(c("h_aft", "cl_aft", "id_aft", "comp", "size_aft"))
 
   print("part1")
-  ss <- noChange(aft_comps, bef_comps, hb)
+  ss <- noChange(aft_comps, bef_comps, hbdata)
   
   print("part2")
   hdata <- aft_comps %>% 
@@ -271,10 +96,7 @@ for (h_after in heights[-1]) {
   
   print("part3")
   if (nrow(cc) > 0) {
-    natp2h <- which(is.na(cc$tp2_h))
     cc$actual_growth_rate <- (cc$tp2_cl_size - cc$tp1_cl_size) / cc$tp1_cl_size
-    cc$actual_growth_rate[natp2h] <- cc$acc[natp2h] <- 0
-    
     cc$acc <- cc$num_novs / cc$actual_growth_rate
     cc$acc[which(cc$actual_growth_rate == 0)] <- 0
     
@@ -295,15 +117,13 @@ for (h_after in heights[-1]) {
 
   print("part4")
   alldata <- bind_rows(alldata, hnew)
-  hb <- hnew
+  hbdata <- hnew
   h_before <- h_after
   bef_comps <- aft_comps %>% set_colnames(c("h_bef", "cl_bef", "id_bef", "comp", "size_bef"))
   
-  saveRDS(alldata, "alldata.Rds")
-  saveRDS(hb, "hb.Rds")
-  saveRDS(h_before, "h_before.Rds")
-  saveRDS(bef_comps, "bef_comps.Rds")
+  saveData(dtype=1, a=alldata, hbdata=hbdata, hb=h_before, cb=bef_comps)
 }
 
 # restarted process at 8:22, starting height 5 at 8:42, height 11 at 8:47, height 428 at 9:18, height 630 at 9:29, 
 # height 893 at 9:50, finished at 9:53
+# AN HOUR AND A HALF!!!
--- functions/processing_functions.R
+++ functions/processing_functions.R
@@ -1,122 +1,178 @@
-saveData <- function(dtype = 1, h = NULL, sh = NULL, sw = NULL, m = NULL, transit, flagged) {
+
+trackClusters <- function(hdata, t2_comps) {
+  # FOUND A PROBLEM WITH USING GREP!
+  #   - if you're looking for composition 1, it considers 10, 107, 1247, ... as hits
+  #   - need to include the commas in our actual checks
+  
+  singletons <- hdata %>% filter(tp1_cl_size == 1)
+  t1set <- t2set <- tibble()
+  if (nrow(singletons) > 0) { # at least one singleton cluster
+    # TP1 clusters that contain these TP1 singletons for this height
+    a1 <- t1_coded %>% filter(id %in% singletons$id) %>% rename(tp1_id = id)
+    # the TP2 clusters that contain at least the TP1 isolates from the above TP1 clusters
+    a2 <- t2_coded %>% filter(isolate %in% a1$isolate) %>% rename(tp2_id = id)
+    # the TP1 singletons matched to TP2 clusters that contain at least those genomes
+    a3 <- left_join(a1, a2, by = "isolate") %>% select(tp1_id, tp2_id)
+    
+    a4 <- t2_comps %>% filter(id %in% a3$tp2_id) %>% 
+      left_join(., a3, by = c("id" = "tp2_id")) %>% 
+      select(-composition, -id)
+    t1set <- singletons %>% rename(tp1_id = id) %>% 
+      select(-composition) %>% 
+      right_join(., a4, by = "tp1_id") %>% select(-tp1_id)
+  }
+  
+  hx <- hdata %>% filter(tp1_cl_size > 1)
+  if (nrow(hx) > 0) { # at least one cluster with size larger than 1
+    pb <- txtProgressBar(min = 0, max = nrow(hx) + 1, initial = 0, style = 3)
+    k <- 0
+    t2set <- lapply(1:nrow(hx), function(i) {
+      k <- k + i
+      setTxtProgressBar(pb, k)
+      cluster_i <- hx[i,]
+      
+      if (nchar(cluster_i$composition) > 2000) {
+        results_i <- checkEachIsolate(cluster_i, t2_coded, t2_comps)
+      }else {
+        results_i <- t2_comps[grep(cluster_i$composition, t2_comps$composition),] %>% 
+          rmIDComp() %>% bind_cols(cluster_i, .) %>% rmIDComp()
+        if (nrow(results_i) == 0) {
+          results_i <- checkEachIsolate(cluster_i, t2_coded, t2_comps)
+        }
+      }
+      return(results_i)
+    }) %>% bind_rows()
+    setTxtProgressBar(pb, k + 1)
+    close(pb)
+    if (any(is.na(t2set))) {message("\nNA values!\n")}
+  }
+  
+  if (nrow(t1set) > 0 & nrow(t2set) > 0) {
+    bind_rows(t1set, t2set) %>% return()
+  }else if (nrow(t1set) == 0 & nrow(t2set) > 0) {
+    t2set %>% return()
+  }else if (nrow(t1set) > 0 & nrow(t2set) == 0) {
+    t1set %>% return()
+  }else { # nrow(t1set) == 0 & nrow(t2set) == 0
+    message("No results!")
+  }
+}
+
+saveData <- function(dtype = 1, a = NULL, hbdata = NULL, hb = NULL, 
+                     cb = NULL, tp = NULL, tpdata = NULL) {
   if (!dir.exists("outputs")) {
     dir.create("outputs/height_data", recursive = TRUE)
-    # dir.create("outputs/transit", recursive = TRUE)
+    dir.create("outputs/transit", recursive = TRUE)
   }
+  
   if (dtype == 1) {
-    paste0("outputs/height_data/h_", h, ".Rds") %>% saveRDS(sh, .)
+    saveRDS(a, "outputs/transit/alldata.Rds")
+    saveRDS(hbdata, "outputs/transit/hbdata.Rds")
+    saveRDS(hb, "outputs/transit/h_before.Rds")
+    saveRDS(cb, "outputs/transit/bef_comps.Rds")
   }else if (dtype == 2) {
-    saveRDS(sw, "outputs/stopwatch.Rds")
-    message("\nSaved stopwatch data.")
-  }else if (dtype == 3) {
-    mergeResults("outputs/height_data/") %>% saveRDS(., "outputs/results.Rds")
-    message("\nSaved transitory results data - now need to run preparingmetrics.R")
-  }else if (dtype == 4) {
-    write.csv(m, "outputs/all_clusters_table.csv", row.names = FALSE)
-    write.csv(m[1:10,], "outputs/first_ten_rows.csv", row.names = FALSE)
-    message("\nSaved metrics to outputs folder.")
-    # }else if (dtype == 5) {
-    #   saveRDS(transit[['i']], paste0("outputs/transit/ids/h_", h, ".Rds"))
-    #   saveRDS(transit[['po']], paste0("outputs/transit/postcc//h_", h, ".Rds"))
-    #   saveRDS(transit[['pr']], paste0("outputs/transit/precc//h_", h, ".Rds"))
-    #   saveRDS(transit[['m']], paste0("outputs/transit/metrics/h_", h, ".Rds"))
-  }else if (dtype == 6) {
-    write.csv(flagged, "outputs/flagged_cases_w_isolates.csv", row.names = FALSE)
-  }else if (dtype == 7) {
-    write.csv(flagged, "outputs/flagged_cases_just_clusters.csv", row.names = FALSE)
+    saveRDS(tpdata, paste0("t", tp, "_comps.Rds"))
   }
-}
-
-clusterIDS <- function(dataset, dtype = 2) {
-  df <- dataset %>% 
-    meltData(., "isolate") %>% 
-    factorToInt("variable") %>% 
-    set_colnames(c("isolate", "tp2_h", "tp2_cl")) %>% 
-    createID(., "tp2_h", "tp2_cl")
   
-  if (dtype == 1) {
-    df %>% select(-isolate) %>% unique() %>% 
-      arrange(tp2_h, tp2_cl) %>% return()
-  }else {
-    df %>% arrange(tp2_h, tp2_cl) %>% return()
-  }
+  # if (dtype == 1) {
+  #   paste0("outputs/height_data/h_", h, ".Rds") %>% saveRDS(sh, .)
+  # }else if (dtype == 2) {
+  #   saveRDS(sw, "outputs/stopwatch.Rds")
+  #   message("\nSaved stopwatch data.")
+  # }else if (dtype == 3) {
+  #   mergeResults("outputs/height_data/") %>% saveRDS(., "outputs/results.Rds")
+  #   message("\nSaved transitory results data - now need to run preparingmetrics.R")
+  # }else if (dtype == 4) {
+  #   write.csv(m, "outputs/all_clusters_table.csv", row.names = FALSE)
+  #   write.csv(m[1:10,], "outputs/first_ten_rows.csv", row.names = FALSE)
+  #   message("\nSaved metrics to outputs folder.")
+  # }else if (dtype == 6) {
+  #   write.csv(flagged, "outputs/flagged_cases_w_isolates.csv", row.names = FALSE)
+  # }else if (dtype == 7) {
+  #   write.csv(flagged, "outputs/flagged_cases_just_clusters.csv", row.names = FALSE)
+  # }
 }
-
-# --------------------------------------------------------------------------------------------------------------
-# time2_data <- df
-# all_clusters <- ac
-# df1 <- meltedTP1
-# ids <- ids
-# jo <- just_originals
-resultsProcess <- function(time2_data, all_clusters, df1, ids, jo) {
-  # given the heights and cluster assignments for all TP1 isolates, we filter to keep only the genomes found 
-  # in a particular cluster kc, then we identify all the clusters at TP1 that only contain these genomes
-  # and then return the first height-and cluster pair where these isolates are found in a single cluster  
-  
-  # all the jo clusters should have at least one original isolate
-  # note that the ac (all_clusters) list just indicates which clusters changed in composition
-  #   - it is possible for one or more to contain only novels
-  #     - these will be ignored for now, and dealt with in a separate step
-  # pb <- progress_bar$new(total = length(all_clusters))
-  ids_copy <- ids
-  a <- list()
-  for (i in 1:length(all_clusters)) {
-    # pb$tick()
-    # print(paste0("h_", h, "-", i, "/", length(all_clusters)))
-    kc <- jo %>% dplyr::filter(tp2_cl == all_clusters[i])
-    
-    # this is true if the cluster actually has original isolates, and is not just made of novels
-    # if it is just novels, we can skip it for now, it will be dealt with later
-    if (nrow(kc) > 0) {
-      # the first cluster in the TP1 dataset to contain only the originals found in the 
-      # TP2 cluster, cluster_x, in form || tp1_h | tp1_cl | tp1_cl_size ||
-      x1 <- df1 %>% 
-        filter(isolate %in% kc$isolate) %>% 
-        select(-isolate) %>%
-        countCases(., "tp1_cl_size") %>% 
-        filter(tp1_cl_size == nrow(kc)) %>% 
-        slice(n = 1)
-      
-      # We add this "originating cluster" to the dataframe with this TP2 cluster.
-      # If this "originating cluster" is already in the ID list, return with an unflagged notation, 
-      # otherwise add to the list of IDs and flag the cluster.
-      # NOTE: if we use an lapply, any IDs added to the list during this particular lapply 
-      # are not compared to new IDs --> so if you add a new ID for a cluster, it can still be 
-      # added for subsequent clusters for the same height.
-      # 
-      # This can only happen in cases where a cluster was larger at TP1, where these original isolates 
-      # were actually clustered into two or more different clusters at TP1.
-      # e.g. cluster 7-1310-TP1 (size 5), clustered into 1-1777-TP2 (size 3) and 1-1778-TP2 (size 2).
-      # As this is just something that can happen with clustering, we will not make an exception in the 
-      # flagging methodology --> will run as a for-loop so we can flag the secondary cases as "seen before", 
-      # even if it was seen before at an earlier cluster for the same height
-      # 
-      # NOTE: need to update a copy of the ID list as well, so you can see when a cluster was flagged before, 
-      # even if it was just at the same height
-      verify_ID <- paste0(x1$tp1_h, "-", x1$tp1_cl) %>% checkID(., kc, x1, ids_copy)
-      ids_copy <- verify_ID %>% '[['("new_ids")
-      a[[i]] <- verify_ID %>% '[['("new_kc")  
-    }
-  }
-  bind_rows(a) %>% return()
-}
-
-# cluster 7-1310 (is the TP1 originating cluster for both 1-1777 and 1-1778 at TP2)
-# it is flagged for both, when it should only be flagged for 1-1777 --> regardless of the change in cluster 
-# size, new clusters should only be flagged once per originating cluster
-
-# df <- time2_raw
-# df_coded <- time2_coded
-# h <- height2
-# novel_isolates <- novels
-# precomp <- precc
-oneHeight <- function(df, df_coded, h, novel_isolates, meltedTP1, ids, ac, precomp) {
-  # we find the cluster assignments for all isolates at a particular height at TP2 that can be 
-  # found in the given list of clusters (have not yet found originating clusters for these)
-  # dataset of form || isolate (originals) | tp2 height | tp2 cluster | tp2 cluster size ||
-  just_originals <- clusterAssignments(h, df, ac, novel_isolates)
-  # "just_originals" only includes the original isolates (but still has the actual TP2 cluster sizes) and 
-  # is a dataset of form || isolate | height (at tp2) | cluster (at tp2) | cluster size (at tp2) ||
-  pull(precomp, h_before) %>% collectionMsg(h, df, .)
-  resultsProcess(df, ac, meltedTP1, ids, just_originals) %>% return()
-}
+# 
+# clusterIDS <- function(dataset, dtype = 2) {
+#   df <- dataset %>% 
+#     meltData(., "isolate") %>% 
+#     factorToInt("variable") %>% 
+#     set_colnames(c("isolate", "tp2_h", "tp2_cl")) %>% 
+#     createID(., "tp2_h", "tp2_cl")
+#   
+#   if (dtype == 1) {
+#     df %>% select(-isolate) %>% unique() %>% 
+#       arrange(tp2_h, tp2_cl) %>% return()
+#   }else {
+#     df %>% arrange(tp2_h, tp2_cl) %>% return()
+#   }
+# }
+# 
+# # --------------------------------------------------------------------------------------------------------------
+# resultsProcess <- function(time2_data, all_clusters, df1, ids, jo) {
+#   # given the heights and cluster assignments for all TP1 isolates, we filter to keep only the genomes found 
+#   # in a particular cluster kc, then we identify all the clusters at TP1 that only contain these genomes
+#   # and then return the first height-and cluster pair where these isolates are found in a single cluster  
+#   
+#   # all the jo clusters should have at least one original isolate
+#   # note that the ac (all_clusters) list just indicates which clusters changed in composition
+#   #   - it is possible for one or more to contain only novels
+#   #     - these will be ignored for now, and dealt with in a separate step
+#   # pb <- progress_bar$new(total = length(all_clusters))
+#   ids_copy <- ids
+#   a <- list()
+#   for (i in 1:length(all_clusters)) {
+#     # pb$tick()
+#     # print(paste0("h_", h, "-", i, "/", length(all_clusters)))
+#     kc <- jo %>% dplyr::filter(tp2_cl == all_clusters[i])
+#     
+#     # this is true if the cluster actually has original isolates, and is not just made of novels
+#     # if it is just novels, we can skip it for now, it will be dealt with later
+#     if (nrow(kc) > 0) {
+#       # the first cluster in the TP1 dataset to contain only the originals found in the 
+#       # TP2 cluster, cluster_x, in form || tp1_h | tp1_cl | tp1_cl_size ||
+#       x1 <- df1 %>% 
+#         filter(isolate %in% kc$isolate) %>% 
+#         select(-isolate) %>%
+#         countCases(., "tp1_cl_size") %>% 
+#         filter(tp1_cl_size == nrow(kc)) %>% 
+#         slice(n = 1)
+#       
+#       # We add this "originating cluster" to the dataframe with this TP2 cluster.
+#       # If this "originating cluster" is already in the ID list, return with an unflagged notation, 
+#       # otherwise add to the list of IDs and flag the cluster.
+#       # NOTE: if we use an lapply, any IDs added to the list during this particular lapply 
+#       # are not compared to new IDs --> so if you add a new ID for a cluster, it can still be 
+#       # added for subsequent clusters for the same height.
+#       # 
+#       # This can only happen in cases where a cluster was larger at TP1, where these original isolates 
+#       # were actually clustered into two or more different clusters at TP1.
+#       # e.g. cluster 7-1310-TP1 (size 5), clustered into 1-1777-TP2 (size 3) and 1-1778-TP2 (size 2).
+#       # As this is just something that can happen with clustering, we will not make an exception in the 
+#       # flagging methodology --> will run as a for-loop so we can flag the secondary cases as "seen before", 
+#       # even if it was seen before at an earlier cluster for the same height
+#       # 
+#       # NOTE: need to update a copy of the ID list as well, so you can see when a cluster was flagged before, 
+#       # even if it was just at the same height
+#       verify_ID <- paste0(x1$tp1_h, "-", x1$tp1_cl) %>% checkID(., kc, x1, ids_copy)
+#       ids_copy <- verify_ID %>% '[['("new_ids")
+#       a[[i]] <- verify_ID %>% '[['("new_kc")  
+#     }
+#   }
+#   bind_rows(a) %>% return()
+# }
+# 
+# # cluster 7-1310 (is the TP1 originating cluster for both 1-1777 and 1-1778 at TP2)
+# # it is flagged for both, when it should only be flagged for 1-1777 --> regardless of the change in cluster 
+# # size, new clusters should only be flagged once per originating cluster
+# 
+# oneHeight <- function(df, df_coded, h, novel_isolates, meltedTP1, ids, ac, precomp) {
+#   # we find the cluster assignments for all isolates at a particular height at TP2 that can be 
+#   # found in the given list of clusters (have not yet found originating clusters for these)
+#   # dataset of form || isolate (originals) | tp2 height | tp2 cluster | tp2 cluster size ||
+#   just_originals <- clusterAssignments(h, df, ac, novel_isolates)
+#   # "just_originals" only includes the original isolates (but still has the actual TP2 cluster sizes) and 
+#   # is a dataset of form || isolate | height (at tp2) | cluster (at tp2) | cluster size (at tp2) ||
+#   pull(precomp, h_before) %>% collectionMsg(h, df, .)
+#   resultsProcess(df, ac, meltedTP1, ids, just_originals) %>% return()
+# }
