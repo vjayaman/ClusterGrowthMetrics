e04144f76b0b6406e4f3f7ad2c7ac57356a6d504
vjayaman <vjayaman314@gmail.com>
2021-02-08 09:36:23 -0600

    Update and organize relevant functions

---
+++ functions/formattingdata.R
@@ -0,0 +1,109 @@
+
+# assertthat::see_if(identical(a1, a2), msg = paste0("File ", j, " is not the same in both directories."))
+timeTaken <- function(pt, sw) {
+  t1 <- (sw[['end_time']] - sw[['start_time']])/60/60
+  t2 <- abs(t1 - trunc(t1))*60
+  t3 <- abs(t2 - trunc(t2))*60
+  
+  word1 <- if_else(trunc(t1) == 1, "hour", "hours")
+  word2 <- if_else(trunc(t2) == 1, "minute", "minutes")
+  word3 <- if_else(trunc(t3) == 1, "second", "seconds")
+  
+  paste0("  The ", pt, " process took ", trunc(t1), " ", word1, ", ", 
+         trunc(t2), " ", word2, " and ", trunc(t3), " ", word3, ".\n") %>% return()
+}
+
+outputDetails <- function(msg, newcat = FALSE) {
+  cat(msg)
+  if (newcat) {cat("\n")}
+  message(msg)
+}
+
+createID <- function(df, c0, c1, c2) {
+  df %>% add_column(id = paste0(toupper(c0), "_h", pull(df, c1), "_c", pull(df, c2))) %>% return()
+}
+
+rmIDComp <- function(df) {df %>% select(-id, -composition) %>% return()}
+
+meltData <- function(dataset, id_val) {melt(dataset, id = id_val) %>% as_tibble() %>% return()}
+
+factorToInt <- function(dataset, cname) {
+  dataset %>% mutate(across(all_of(cname), as.character)) %>% 
+    mutate(across(all_of(cname), as.integer)) %>% return()
+}
+
+charToInt <- function(x, v) {
+  gsub(v, "", x) %>% as.integer()
+}
+
+codeIsolates <- function(df, tpx, all_iso) {
+  hx <- paste0(tpx, "_h")
+  cx <- paste0(tpx, "_cl")
+  
+  df %>% right_join(all_iso, ., by = c("char_isolate" = "isolate")) %>% 
+    select(-char_isolate) %>% rename(isolate = num_isolate) %>% melt(id = "isolate") %>% 
+    as_tibble() %>% set_colnames(c("isolate", hx, cx)) %>% factorToInt(., hx) %>% 
+    createID(., tpx, hx, cx) %>% mutate(isolate = paste0("-", isolate, "-")) %>% return()
+}
+
+meltedSizing <- function(df, y) {
+  tp <- paste0("tp", y, "_")
+  # the TPX cluster assignments
+  tp_melted <- df %>% melt(id = "isolate") %>% as_tibble() %>% 
+    factorToInt(., "variable") %>% 
+    createID(., paste0("tp", y), "variable", "value") %>% 
+    set_colnames(c("isolate", "tp_h", "tp_cl", "tp_id"))
+  
+  # adding a column with the TPX cluster sizes
+  tp_melted %>% group_by(tp_id) %>% summarise(tp_cl_size = n(), .groups = "drop") %>% 
+    left_join(tp_melted, ., by = "tp_id") %>% 
+    set_colnames(c("isolate", paste0(tp, "h"), paste0(tp, "cl"), 
+                   paste0(tp, "id"), paste0(tp, "cl_size"))) %>% return()
+}
+
+leadingZeros <- function(df, cname, lc, w = NULL) {
+  if (is.null(w)) {w <- df[,cname] %>% max() %>% nchar()}
+  df[,cname] <- pull(df, cname) %>% formatC(., width = w, format = "d", flag = "0") %>% paste0(lc, .)
+  return(df)
+}
+
+# given the defining filename, read in the data (need the full path from your working directory)
+readBaseData <- function(filename, file_number) {
+  if (is.na(filename)) {
+    stop(paste0("Time point ", file_number, " dataset not found."))
+  }else {
+    read.csv(file = filename, stringsAsFactors = FALSE, numerals = "no.loss", 
+             check.names = FALSE, sep = "\t") %>% as_tibble() %>% return()
+  }
+}
+
+resultFiles <- function(df, op, heights) {
+  clusters_formatted <- df %>% set_colnames(c(
+    "TP1 ID", "TP1 height", "TP1 cluster", "TP1 cluster size", "First time this cluster was seen in TP1", 
+    "Last time this cluster was seen in TP1", "First time this cluster was seen in TP2", "TP2 height", 
+    "TP2 cluster", "TP2 cluster size", "Number of additional TP1 strains in the TP2 match", 
+    "Number of novels in the TP2 match", "Actual cluster size change (TP2 size - TP1 size)",
+    "Actual growth rate = (TP2 size - TP1 size) / (TP1 size)", "Number of novels / Actual growth rate", 
+    "Novel growth = (TP2 size) / (TP2 size - number of novels)"))
+  
+  write.csv(clusters_formatted, paste0(op,"TP1_cluster_results.csv"), row.names = FALSE)
+  
+  isolates_formatted <- time1_raw %>% select(isolate, as.character(heights)) %>% 
+    melt(., id = "isolate") %>% as_tibble() %>% 
+    rename(tp1_h = variable, tp1_cl = value) %>% 
+    mutate(across(tp1_h, as.character)) %>% mutate(across(tp1_h, as.integer)) %>% 
+    leadingZeros(., "tp1_h", "h") %>% leadingZeros(., "tp1_cl", "c") %>% 
+    right_join(., df, by = c("tp1_h", "tp1_cl")) %>% 
+    arrange(tp1_h, tp1_cl, tp2_h, tp2_cl) %>% select(isolate, colnames(df)) %>% 
+    set_colnames(c("Isolates", colnames(clusters_formatted)))
+  
+  write.csv(isolates_formatted, paste0(op, "TP1_strain_results.csv"), row.names = FALSE)
+}
+
+saveData <- function(dtype = 1, tmp = NULL, h = NULL) {
+  if (!dir.exists("outputs")) {dir.create("outputs/height_data", recursive = TRUE)}
+  if (dtype == 3) {
+    saveRDS(tmp, paste0("outputs/height_data/h", h, ".Rds"))
+  }
+}
+
---
+++ functions/tracking_functions.R
@@ -0,0 +1,201 @@
+x <- c("tibble", "magrittr", "dplyr", "reshape2", "scales", "progress", 
+       "stringr", "ggplot2", "plotly", "optparse", "shiny")
+lapply(x, require, character.only = TRUE)
+source("wallace.R")
+source("running_nawc.R")
+source("formattingdata.R")
+
+compsSet <- function(tp_coded, tp, indicate_progress) {
+  # sort the data and assign general labels
+  cb <- tp_coded %>% arrange(tp_h, tp_cl, isolate) %>% set_colnames(c("isolate", "tp_h", "tp_cl", "id"))
+  allheights <- cb$tp_h %>% unique()
+  
+  # This part returns a data frame of CLUSTER ID | CLUSTER COMPOSITION | CLUSTER SIZE, 
+  # for all heights and clusters at the given time point
+  if (indicate_progress) {pb <- txtProgressBar(min = 0, max = length(allheights), initial = 0, style = 3)}
+  tmp <- lapply(1:length(allheights), function(i) {
+    if (indicate_progress) {setTxtProgressBar(pb, i)}
+    
+    # for height i, we first collect the data from tp_coded, then arrange by isolate
+    x1 <- cb %>% filter(tp_h == allheights[i]) %>% arrange(isolate)
+    x <- x1 %>%
+      add_column(num_iso = x1$isolate %>% gsub("-", "", .) %>% as.integer()) %>%
+      arrange(num_iso)
+    
+    # if at a height there is only one cluster, we handle it differently
+    num_clusters <- x$id %>% unique() %>% length()
+    # if there are multiple clusters at a given height:
+    if (num_clusters > 1) {
+      # output a table where each cluster has its isolate composition to the right: 
+      #   isolate 1, isolate 2, isolate 3, ...
+      a2 <- aggregate(isolate ~ id, data = x, FUN = toString) %>% as_tibble() %>% 
+        set_colnames(c("id", "composition"))
+      a3 <- aggregate(isolate ~ id, data = x, FUN = length) %>% as_tibble() %>% 
+        set_colnames(c("id", "size"))
+      left_join(a2, a3, by = "id") %>% return()
+    }else {
+      x$isolate %>% paste0(., collapse=",") %>% 
+        tibble(id = unique(x$id), composition = ., size = length(x$isolate)) %>% return()
+    }
+  }) %>% bind_rows()
+  
+  if (indicate_progress) {close(pb)}
+  
+  # This part uses the cluster IDs to get the height and cluster as separate columns, 
+  # then binds it with the cluster composition table
+  tpcomps <- paste0(c(tp, "_h", "_"), collapse = "|") %>% gsub(., "", tmp$id) %>% 
+    str_split_fixed(., "c", 2) %>% set_colnames(c("tp_h", "tp_cl")) %>% as_tibble() %>% 
+    bind_cols(., tmp) %>% mutate(across(c(tp_h, tp_cl), as.integer))
+  
+  if (tp == "TP1") {
+    tpcomps %>% rename(tp1_h = tp_h, tp1_cl = tp_cl, tp1_cl_size = size) %>% return()
+  }else {
+    tpcomps %>% rename(tp2_h = tp_h, tp2_cl = tp_cl, tp2_cl_size = size) %>% return()
+  }
+}
+
+isolatedHeight <- function(h_before, t1_comps, t2_comps, t2_colnames, t1_coded, t2_coded, ind_p) {
+  cc <- t1_comps %>% filter(tp1_h == h_before) %>% arrange(tp1_h, tp1_cl) %>% 
+    trackClusters(., t2_comps, t2_colnames, t1_coded, t2_coded, indicate_progress = ind_p) %>%
+    createID(., "tp1", "tp1_h", "tp1_cl")
+  
+  x1 <- t1_comps %>% filter(id %in% cc$id) %>% select(id, composition)
+  x2 <- t1_comps %>% filter(composition %in% x1$composition) %>% 
+    arrange(tp1_h, tp1_cl) %>% group_by(composition) %>% 
+    filter(row_number() == 1) %>% select(id, composition) %>% 
+    set_colnames(c("flag", "composition")) %>% ungroup()
+  
+  left_join(x1, x2, by = "composition") %>% select(-composition) %>% 
+    left_join(cc, ., by = "id") %>% return()
+}
+
+noChange <- function(ca, cb, hb) {
+  in_both_heights <- ca %>% 
+    inner_join(., cb, by = "comp") %>% 
+    select(h_aft, cl_aft, size_aft, id_bef, id_aft)
+  
+  stayed_the_same <- left_join(in_both_heights, hb, by = c("id_bef" = "id")) %>% 
+    select(-tp1_h, -tp1_cl, -tp1_cl_size, -id_bef) %>% 
+    rename(tp1_h=h_aft, tp1_cl=cl_aft, tp1_cl_size=size_aft, id=id_aft) %>% 
+    select(colnames(hb))
+  # stayed_the_same$flagged_heights <- stayed_the_same$flagged_heights + 1  
+  
+  # not found in TP2 in some capacity
+  inds <- which(is.na(stayed_the_same$tp2_h))
+  if (length(inds) > 0) {stop("Did not stay the same")}
+  # stayed_the_same$tp2_cl_size[inds] <- stayed_the_same$num_novs[inds] <- 0
+  return(stayed_the_same)
+}
+
+checkEachIsolate <- function(cluster_i, t2_coded, t2_comps) {
+  # no matching TP2 cluster found - the order of the cluster composition is muddling this up
+  if (length(grep(" ", cluster_i$composition)) == 0) {
+    isolates <- strsplit(cluster_i$composition, split = ",") %>% unlist()
+  }else {
+    isolates <- strsplit(cluster_i$composition, split = ", ") %>% unlist()  
+  }
+  ids <- t2_coded %>% filter(isolate %in% isolates) %>%
+    pull(id) %>% table() %>% 
+    as.data.frame() %>% as_tibble() %>% set_colnames(c("id", "size")) %>% 
+    filter(size == length(isolates)) %>% pull(id)
+  df2 <- t2_comps %>% filter(id %in% ids)
+  
+  cluster_i %>% rmIDComp() %>% bind_cols(., df2) %>% rmIDComp() %>% return()
+}
+
+# When using grep: if looking for 1, will return 1, 10, 214, etc. So we sandwich with hyphens: -1-,-10-,...
+trackClusters <- function(hdata, t2_comps, t2names, t1_coded, t2_coded, indicate_progress) {
+  
+  singletons <- hdata %>% filter(tp1_cl_size == 1)
+  t1set <- t2set <- tibble()
+  if (nrow(singletons) > 0) { # at least one singleton cluster
+    isos_in_tp1 <- t1_coded %>% filter(id %in% singletons$id) %>% rename(tp1_id = id)
+    
+    a3 <- t2_coded %>% filter(isolate %in% isos_in_tp1$isolate) %>% 
+      rename(tp2_id = id) %>% left_join(isos_in_tp1, ., by = "isolate") %>% select(-isolate)
+    
+    a4 <- singletons %>% select(id, tp1_cl_size) %>% rename(tp1_id = id) %>% left_join(a3, ., by = "tp1_id")
+    
+    # basic TP1 data | basic TP2 data | number novels
+    # i.e. the singletons are tracked to the TP2 clusters they're found in
+    t1set <- t2_comps %>% select(id, tp2_cl_size, num_novs) %>% rename(tp2_id = id) %>% 
+      left_join(a4, ., by = "tp2_id") %>% select(tp1_h, tp1_cl, tp1_cl_size, tp2_h, tp2_cl, tp2_cl_size, num_novs)
+  }
+  
+  hx <- hdata %>% filter(tp1_cl_size > 1)
+  if (nrow(hx) > 0) { # at least one cluster with size larger than 1
+    if (indicate_progress) {
+      tc <- txtProgressBar(min = 0, max = nrow(hx), initial = 0, style = 3)  
+    }
+    t2set <- lapply(1:nrow(hx), function(i) {
+      if (indicate_progress) {setTxtProgressBar(tc, i)}
+      cluster_i <- hx[i,]
+      
+      if (nchar(cluster_i$composition) > 2000) {
+        results_i <- checkEachIsolate(cluster_i, t2_coded, t2_comps)
+      }else {
+        inds <- grep(cluster_i$composition, t2_comps$composition)
+        results_i <- t2_comps[inds,] %>% rmIDComp() %>% bind_cols(cluster_i, .) %>% rmIDComp()
+        
+        if (nrow(results_i) == 0) {
+          results_i <- checkEachIsolate(cluster_i, t2_coded, t2_comps)
+        }
+      }
+      # if the process is being cut off partway
+      # not tracking to all heights (e.g. TP2 height 1634 has not been found)
+      if (!(last(t2names) %in% results_i$tp2_h)) {
+        results_i <- checkEachIsolate(cluster_i, t2_coded, t2_comps)
+      }
+      results_i %>% arrange(tp2_h, tp2_cl) %>% return()
+      
+    }) %>% bind_rows()
+    if (indicate_progress) {close(tc)}
+    if (any(is.na(t2set))) {message("\nNA values!\n")}
+  }
+  
+  bind_rows(t1set, t2set) %>% arrange(tp1_h, tp1_cl) %>% return()
+}
+
+# additional TP1s are the TP1 strains that were not found in the TP1 cluster, 
+# but are found in the TP2 cluster (and are not novels)
+additionalTP1 <- function(b1, b2, id1, id2, novs) {
+  tp1_isos <- b1 %>% filter(tp1_id == id1) %>% pull(isolate)
+  add_tp1 <- b2 %>% filter(tp2_id == id2) %>% pull(isolate) %>% 
+    setdiff(., c(tp1_isos, novs)) %>% length()
+  return(tibble(tp1_id = id1, tp2_id = id2, add_tp1))
+}
+
+oneHeight <- function(h_i, novels, t2_composition, t1_composition, oneh, b1, b2) {
+  # check: (no cluster numbers skipped) - none should be skipped by definition, but just in case
+  # identical(unique(oneh$tp1_cl), min(oneh$tp1_cl):max(oneh$tp1_cl))
+  
+  # first TP2 match for each TP1 cluster at this height
+  d2 <- oneh[diff(c(0, oneh$tp1_cl)) != 0,] %>% rename(tp1_id = id) %>% 
+    createID(., "tp2", "tp2_h", "tp2_cl") %>% rename(tp2_id = id)
+  matched <- d2 %>% select(tp1_id, tp2_id)
+  
+  e2 <- t2_composition %>% select(id, composition) %>% rename(tp2_id = id, comp2 = composition)
+  e1 <- t1_composition %>% select(id, composition) %>% rename(tp1_id = id, comp1 = composition) %>% 
+    filter(tp1_id %in% matched$tp1_id) %>% 
+    left_join(., matched, by = "tp1_id") %>% left_join(., e2, by = "tp2_id")
+  
+  did_not_change <- e1 %>% filter(comp1 == comp2) %>% select(tp1_id, tp2_id) %>% add_column(add_tp1 = 0)
+  chg <- e1 %>% filter(comp1 != comp2) %>% select(tp1_id, tp2_id)
+  
+  c1 <- lapply(1:nrow(chg), function(i) {
+    additionalTP1(b1, b2, chg$tp1_id[i], chg$tp2_id[i], novels)
+  }) %>% bind_rows(., did_not_change) %>% left_join(d2, ., by = c("tp1_id", "tp2_id"))
+  
+  c1 %>% add_column(
+    actual_size_change = (c1$tp2_cl_size - c1$tp1_cl_size), 
+    actual_growth_rate = ((c1$tp2_cl_size - c1$tp1_cl_size) / c1$tp1_cl_size) %>% round(., digits = 3),
+    new_growth = (c1$tp2_cl_size / (c1$tp2_cl_size - c1$num_novs)) %>% round(., digits = 3)) %>% 
+    rename(additional_tp1 = add_tp1) %>% 
+    select(tp1_id, tp1_h, tp1_cl, tp1_cl_size, flag, last_flag, tp2_id, tp2_h, tp2_cl, tp2_cl_size, 
+           additional_tp1, num_novs, actual_size_change, actual_growth_rate, new_growth) %>% 
+    arrange(tp1_h, tp1_cl, tp2_h, tp2_cl) %>% 
+    leadingZeros(., "tp1_cl", "c") %>% leadingZeros(., "tp2_cl", "c") %>%
+    leadingZeros(., "tp1_h", "h", w = max(nchar(colnames(time1_raw)[-1]))) %>%
+    leadingZeros(., "tp2_h", "h", w = max(nchar(colnames(time2_raw)[-1]))) %>% return()
+}
+
